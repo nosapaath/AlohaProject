520060f394208186ce026eb18e10020deaa22043
Nosa Paath <adepaath@gmail.com>
2018-10-25 21:00:40 -0700

    scrolling when clicking on nav links

--- js/aloha.js
+++ js/aloha.js
@@ -1,2 +1,26 @@
 
 
+$(function(){
+$("#shop").on("click", function(event){
+  event.preventDefault();
+  $('html, body').animate({scrollTop: $(".boxes").offset().top}, 1000);
+});
+
+
+$("#featured").on("click", function(event){
+  event.preventDefault();
+  $('html, body').animate({scrollTop: $(".product").offset().top}, 1000);
+});
+
+
+$("#updates").on("click", function(event){
+  event.preventDefault();
+  $('html, body').animate({scrollTop: $(".subscribe").offset().top}, 1000);
+});
+
+  $("#about").on("click", function(event){
+    event.preventDefault();
+    $('html, body').animate({scrollTop: $(".banner").offset().top}, 1000);
+  });
+});
+
