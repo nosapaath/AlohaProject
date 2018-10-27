22f48061e6c3cf54e284e53b535b7c6f4f5c6d70
Nosa Paath <adepaath@gmail.com>
2018-10-27 15:16:23 -0700

    scroll down and header fixed

--- js/aloha.js
+++ js/aloha.js
@@ -30,9 +30,17 @@ $("#updates").on("click", function(event){
     event.preventDefault();
     $('#cartCount').html(function(i, val) { return val*1+1 });
     $("#cartCount").show();
-
-
   });
 
+  $( "button, input[type='submit']").on("click", function(event){
+    event.preventDefault();
+    if($(this).siblings().val() !== ""){
+      var email = $(this).siblings().val()
+      alert("Thank you for subscribing, you will receive weekly newsletter at: " + email);
+    } 
+  });
+
+  
+    
 });
 
