5684980cde6fc5f276b2ceb0d71ff68152f984d1
Nosa Paath <adepaath@gmail.com>
2018-10-27 16:25:26 -0700

    flickity change and add to cart

--- js/aloha.js
+++ js/aloha.js
@@ -1,46 +1,49 @@
-$("#cartCount").hide();
+$(function() {
+  $("#cartCount").hide();
+  
 
-$(function(){
-
-$("#shop").on("click", function(event){
-  event.preventDefault();
-  $('html, body').animate({scrollTop: $(".boxes").offset().top}, 1000);
-});
-
-
-$("#featured").on("click", function(event){
-  event.preventDefault();
-  $('html, body').animate({scrollTop: $(".product").offset().top}, 1000);
-});
-
-
-$("#updates").on("click", function(event){
-  event.preventDefault();
-  $('html, body').animate({scrollTop: $(".subscribe").offset().top}, 1000);
-});
-
-  $("#about").on("click", function(event){
+  $("#shop").on("click", function(event) {
     event.preventDefault();
-    $('html, body').animate({scrollTop: $(".banner").offset().top}, 1000);
+    $("html, body").animate({ scrollTop: $(".boxes").offset().top }, 1000);
   });
 
- 
-
-  $( "button, input[type='button']" ).on("click", function(event){
+  $("#featured").on("click", function(event) {
     event.preventDefault();
-    $('#cartCount').html(function(i, val) { return val*1+1 });
+    $("html, body").animate({ scrollTop: $(".product").offset().top }, 1000);
+  });
+
+  $("#updates").on("click", function(event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: $(".subscribe").offset().top }, 1000);
+  });
+
+  $("#about").on("click", function(event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: $(".banner").offset().top }, 1000);
+  });
+
+  $(".addToCart").on("click", function(event) {
+    event.preventDefault();
+    $("#cartCount").html(function(i, val) {
+      return val * 1 + 1;
+    });
     $("#cartCount").show();
   });
 
-  $( "button, input[type='submit']").on("click", function(event){
+  $("button, input[type='submit']").on("click", function(event) {
     event.preventDefault();
-    if($(this).siblings().val() !== ""){
-      var email = $(this).siblings().val()
-      alert("Thank you for subscribing, you will receive weekly newsletter at: " + email);
-    } 
+    if (
+      $(this)
+        .siblings()
+        .val() !== ""
+    ) {
+      var email = $(this)
+        .siblings()
+        .val();
+      alert(
+        "Thank you for subscribing, you will receive weekly newsletter at: " +
+          email
+      );
+    }
   });
-
-  
-    
 });
-
